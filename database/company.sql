cdaeda6af46a994b7a4b8d801ee545adb0384c86
Nathan Dion Schmid <nathandion.schmid@gmail.com>
2025-11-10 16:19:29 +0100

    Fix typo in login page setup link and add SQL schema for company and user tables

---
+++ database/company.sql
@@ -0,0 +1,11 @@
+/*
+
++────────┌──────────┌──────────┌─────────────────+
+|COMP_ID |COMP_NAME |COMP_DESC |COMP_CREATE_DATE |
++────────└──────────└──────────└─────────────────+
+COMP_ID             INT             PRIMARY KEY,
+COMP_NAME           VARCHAR(100)    NOT NULL,
+COMP_DESC           VARCHAR(500)    NOT NULL,
+COMP_CREATE_DATE    DATETIME        DEFAULT CURRENT_TIMESTAMP
+
+*/
